1d7d016a3a4ce50b2f1fd041db32b279ff56172a
bitezhu <zhush1992@163.com>
2017-07-24 13:23:56 +0800

    cover all the transciprt of A gene

---
+++ genesPlot.py
@@ -0,0 +1,107 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+from bioformat import GTF_Line
+
+from genemodel import *
+
+import sys
+#plt.rcParams['axes.labelpad']=10
+plt.rcParams['axes.ymargin'] =0.2 
+
+f=file(sys.argv[1])  ## only support standard gtf format for now
+geneOI = sys.argv[2].strip()  ## geneid or genename of interst
+
+for line in f:
+    gtfline = GTF_Line(line)
+    if gtfline.gene_name() == geneOI or gtfline.gene_id() == geneOI: 
+        pass
+    else:
+        continue
+    chromosome = gtfline.seqname()
+    start = gtfline.start()
+    end = gtfline.end()
+    strand = gtfline.strand()
+    gene_id = gtfline.gene_id()
+    gene_name = gtfline.gene_name()
+    transcript_id = gtfline.transcript_id()
+    transcript_name = gtfline.transcript_name()
+    feature = gtfline.feature()
+    if feature == 'gene':
+        geneid = Gene(chromosome, start, end, strand, gene_id, name=gene_name,attr={'biotype':gtfline.gene_type()})
+    elif feature == "transcript":
+        transcriptobj = transcript(chromosome, start, end, strand,feature,transcript_id,{'biotype':gtfline.gene_type()})
+        geneid.children[transcript_id]=transcriptobj
+    elif feature == "CDS":
+        cdsobj = CDS(chromosome, start, end, strand,transcript_id,{})
+        geneid.children[transcript_id].addCDS(cdsobj)
+    elif feature == "exon":
+        exonobj = Exon(chromosome, start, end, strand,transcript_id,{})
+        geneid.children[transcript_id].addexon(exonobj)
+    elif feature == "UTR":
+        utrobj = UTR(chromosome, start, end, strand,transcript_id,{})
+        geneid.children[transcript_id].addUTR(utrobj)
+    elif feature == "start_codon":
+        cobj = StartCodon(chromosome, start, end, strand,transcript_id,{})
+        geneid.children[transcript_id].addCodon(cobj)
+    elif feature == "stop_codon":
+        cobj = StopCodon(chromosome, start, end, strand,transcript_id,{})
+        geneid.children[transcript_id].addCodon(cobj)
+    else:
+        sys.stderr.write("This is an offending line !!!! \n%s"%line)
+f.close()
+
+def axaddexons(ax,exons,ycoord,strand="+",color='orange'):
+    ax=plt.gca()
+    for s,e in exons:
+        if strand == "+":
+            ax.arrow(s,ycoord,e-s,0,fc=color,ec='gold', lw=0.05,width=0.5,head_width=0.5,head_length=0.3*(e-s),shape='full',length_includes_head=True)
+        else:
+            ax.arrow(e,ycoord,s-e,0,fc=color,ec='gold', lw=0.05,width=0.5,head_width=0.5,head_length=0.3*(e-s),shape='full',length_includes_head=True)
+    return True
+
+def formatxlabels(xcoord):
+    return "%.2fkb"%(xcoord/float(1000))
+
+
+
+
+plt.rcParams[u'ytick.major.pad']=10
+#plt.tick_params('y',pad=20)   # don't work
+fig=plt.figure(figsize=(20,16))
+ax=fig.add_subplot(111)
+txID = []
+for i,transcript in enumerate(geneid.children.keys()):
+    txID.append(transcript)
+    geneid.children[transcript].inferUTRort()
+    geneid.children[transcript].inferUTR()
+    geneid.children[transcript].inferCodons()
+    geneid.addisoform(geneid.children[transcript])
+    axaddexons(ax,geneid.children[transcript].exons,i,strand=geneid.children[transcript].strand)
+    axaddexons(ax,geneid.children[transcript].fp_utr,i,strand=geneid.children[transcript],color='darkviolet')
+    axaddexons(ax,geneid.children[transcript].tp_utr,i,strand=geneid.children[transcript],color='red')
+    ax.hlines(i ,geneid.start-0.1*geneid.length,geneid.end+0.1*geneid.length, linestyle=u'dotted', color='black', linewidth=1)
+
+#ax.yaxis.labelpad =1000
+ax.set_yticks(range(len(txID)))
+ax.set_yticklabels(txID)
+
+xticks = np.linspace(geneid.start-0.1*geneid.length,geneid.end+0.1*geneid.length,6)
+xlabels = map(formatxlabels,xticks)
+ax.set_xticks(xticks)
+ax.set_xticklabels(xlabels)
+ax.yaxis.set_ticks_position('left')
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.set_axis_bgcolor("snow")
+ax.set_xlim(geneid.start-0.1*geneid.length,geneid.end+0.1*geneid.length)
+ax.set_ylim(bottom=-1,top=i+1)
+plt.title(geneOI)
+#plt.tight_layout()
+
+plt.savefig("gene.png",format='png',dpi=500)
+plt.clf()
+plt.close()
